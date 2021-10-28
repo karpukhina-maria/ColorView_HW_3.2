6be5785eea7c8498219238dd09e41cde80f8ecac
Maria Karpukhina <myshka_n@mail.ru>
2021-10-28 23:05:45 +0300

    Initial Commit

---
+++ ColorView_HW_3.2/ColorView_HW_3_2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorView_HW_3_2App.swift
+//  ColorView_HW_3.2
+//
+//  Created by Mary Jane on 28.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorView_HW_3_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorView_HW_3.2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ColorView_HW_3.2
+//
+//  Created by Mary Jane on 28.10.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
