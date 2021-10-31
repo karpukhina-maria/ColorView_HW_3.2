a285f3eacad8f82354cc121ba8b136f55c267348
Maria Karpukhina <myshka_n@mail.ru>
2021-10-31 23:04:41 +0300

    Done

--- ColorView_HW_3.2/ContentView.swift
+++ ColorView_HW_3.2/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var redSliderValue = Double(Int.random(in: 0...255))
+    @State private var greenSliderValue = Double(Int.random(in: 0...255))
+    @State private var blueSliderValue = Double(Int.random(in: 0...255))
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(.lightGray)
+                .ignoresSafeArea()
+            VStack {
+                ColorView(redValue: redSliderValue, greenValue: greenSliderValue, blueValue: blueSliderValue)
+                    .padding(.vertical)
+                VStack {
+                    ColorSliderKit(sliderValue: $redSliderValue, color: .red)
+                    ColorSliderKit(sliderValue: $greenSliderValue, color: .green)
+                    ColorSliderKit(sliderValue: $blueSliderValue, color: .blue)
+                }
+                Spacer()
+            }
+            .padding(.horizontal)
+        }
+    }
+}
+
+struct ColorSliderKit: View {
+    @Binding var sliderValue: Double    
+    let color: Color
+    
+    var body: some View {
+        HStack {
+            Text("\(lround(sliderValue))").fontWeight(.bold).foregroundColor(.white).frame(width: 40.0)
+            Slider(value: $sliderValue, in: 0...255, step: 1)
+                .accentColor(color)
+                .padding(.horizontal)
+            TextField("", value: $sliderValue, format: .number)
+                .frame(width: 50.0).textFieldStyle(.roundedBorder)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct ColorView: View {
+    let redValue: Double
+    let greenValue: Double
+    let blueValue: Double
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 15)
+            .frame(height: 200.0)
+            .overlay(RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color.white, lineWidth: 3)
+            )
+            .foregroundColor(Color(red: redValue/255, green: greenValue/255, blue: blueValue/255))
     }
 }
 
@@ -19,3 +68,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
